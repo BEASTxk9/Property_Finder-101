34023f1b348bb3c74bd67926154ea063061a47b4
shane <shanestevensxk9@gmail.com>
2022-06-28 15:38:24 +0200

    update

---
+++ java/code.js
@@ -0,0 +1,345 @@
+// document.getElementById("card1").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/008-592x444.jpg" width="80%">
+// <h3>Test1</h3>
+// <h5>Life Choices Academy</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1
+// </p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+// </div>
+// `;
+
+// document.getElementById("card2").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/026-592x444.jpg" width="80%">
+// <h3>Design Apartment</h3>
+// <h5>Quincy st, Brooklyn, NY, USA</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1
+// </p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Mike Moore</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+
+// </div>
+// `;
+
+// document.getElementById("card3").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/012-592x444.jpg" width="80%">
+// <h3>Test1</h3>
+// <h5>Life Choices Academy</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1</p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+
+// </div>
+// `;
+
+// document.getElementById("card4").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/018-592x444.jpg" width="80%">
+// <h3>Test1</h3>
+// <h5>Life Choices Academy</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1</p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+
+// </div>
+// `;
+
+// document.getElementById("card5").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/031-592x444.jpg" width="80%">
+// <h3>Test1</h3>
+// <h5>Life Choices Academy</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1</p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+
+// </div>
+// `;
+
+// document.getElementById("card6").innerHTML = `
+// <div class="text-start">
+
+// <img src="./image/038-592x444.jpg" width="80%">
+// <h3>Test1</h3>
+// <h5>Life Choices Academy</h5>
+// <p><i class="fa-solid fa-bed"></i>: 3 
+// <i class="fa-solid fa-shower"></i>: 2  
+// <i class="fa-solid fa-car"></i>: 1</p>
+// <p><b>Apartment</b></p>
+// <hr>
+// <div class="containetr">
+// <div class="row">
+
+// <div class="col-6">
+// <p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+// </div>
+
+// <div class="col-6">
+// <p><i class="fa-solid fa-link"></i>2 years ago</p>
+// </div>
+
+// </div>
+// </div>
+
+// </div>
+// `;
+
+const properties= [
+    {
+        id: 1,
+        image: "./image/008-592x444.jpg",
+        title: "New Apartment",
+        price: '$125,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 2,
+        baths: 2,
+        parking: 2,
+        area: 1987,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+        
+    },
+    {
+        id: 2,
+        image: "./image/012-592x444.jpg",
+        title: "Apartment For Sale",
+        price: '$876,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 3,
+        baths: 2,
+        parking: 2,
+        area: 2541,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 3,
+        image: "./image/018-592x444.jpg",
+        title: "Renovated Apartment",
+        price: '$485,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 4,
+        baths: 2,
+        parking: 2,
+        area: 3650,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 4,
+        image: "./image/020-592x444.jpg",
+        title: "Apartment Complex",
+        price: '$567,000',
+        address: "7900 Northwest 27th Avenue, Miami, FL, USA",
+        beds: 2,
+        baths: 2,
+        parking: 2,
+        area: 2345,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 5,
+        image: "./image/008-592x444.jpg",
+        title: "New Apartment",
+        price: '$125,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 2,
+        baths: 2,
+        parking: 2,
+        area: 1987,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+        
+    },
+    {
+        id: 6,
+        image: "./image/012-592x444.jpg",
+        title: "Apartment For Sale",
+        price: '$876,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 3,
+        baths: 2,
+        parking: 2,
+        area: 2541,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 7,
+        image: "./image/018-592x444.jpg",
+        title: "Renovated Apartment",
+        price: '$485,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 4,
+        baths: 2,
+        parking: 2,
+        area: 3650,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 8,
+        image: "./image/020-592x444.jpg",
+        title: "Apartment Complex",
+        price: '$567,000',
+        address: "7900 Northwest 27th Avenue, Miami, FL, USA",
+        beds: 2,
+        baths: 2,
+        parking: 2,
+        area: 2345,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 9,
+        image: "./image/018-592x444.jpg",
+        title: "Renovated Apartment",
+        price: '$485,000',
+        address: "6701 South Dixie Highway, Miami, FL, USA",
+        beds: 4,
+        baths: 2,
+        parking: 2,
+        area: 3650,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+    {
+        id: 10,
+        image: "./image/020-592x444.jpg",
+        title: "Apartment Complex",
+        price: '$567,000',
+        address: "7900 Northwest 27th Avenue, Miami, FL, USA",
+        beds: 2,
+        baths: 2,
+        parking: 2,
+        area: 2345,
+        type: "Apartment",
+        years: "2 years",
+        name: "John James"
+    },
+];
+
+
+var title = document.querySelectorAll('#card');
+
+const myJSON = JSON. stringify(properties);
+let container = document.getElementById("somet")
+
+console.log(properties[0])
+
+for(var i = 0; i < properties.length; i++){
+  container.innerHTML += `
+  <div class="p-3">
+        <div id="card" class="p-3">
+        <img src="${properties[i].image}" width="40%">
+        <h5>${properties[i].title}</h5>
+        <p>${properties[i].address}</p>
+        <p><i class="fa-solid fa-bed"></i>: ${properties[i].beds} 
+        <i class="fa-solid fa-shower"></i>: ${properties[i].baths} 
+         <i class="fa-solid fa-car"></i>: ${properties[i].parking} 
+         <i class="bi bi-rulers"></i>: ${properties[i].area} sq ft
+         </p>
+         <p><b>${properties[i].type}</b></p>
+         <hr>
+         <div class="row">
+         <div class="col-6 text-start"><p><i class="fa-solid fa-user"></i> ${properties[i].name}</p></div>
+         <div class="col-6 text-end"><p><i class="fa-solid fa-link"></i> ${properties[i].years}</p></div>
+         </div>
+
+
+        </div>
+      </div>`
+};
